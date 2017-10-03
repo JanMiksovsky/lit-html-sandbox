4b7acd5fb9c30002c407ea40e8c6b9c7481f646e
Jan Miksovsky <jan@quickui.org>
2017-10-03 16:13:03 -0700

    shadowCreated method lets component do initialization based on rendered shadow tree.

--- src/mixins/LitHtmlShadowMixin.js
+++ src/mixins/LitHtmlShadowMixin.js
@@ -9,12 +9,21 @@ export default function LitHtmlShadowMixin(Base) {
   return class LitHtmlShadow extends Base {
     render() {
       if (super.render) { super.render(); }
+      let newShadow = false;
       if (!this.shadowRoot) {
         // Initial render; create shadow.
         this.attachShadow({ mode: 'open' });
+        newShadow = true;
       }
+
       // Invoke lit-html to render the shadow subtree.
       render(this.template, this.shadowRoot);
+
+      // If we've created a new shadow, let the component do other
+      // initialization based on the rendered shadow tree.
+      if (newShadow && this[symbols.shadowCreated]) {
+        this[symbols.shadowCreated]();
+      }
     }
   }
 }
