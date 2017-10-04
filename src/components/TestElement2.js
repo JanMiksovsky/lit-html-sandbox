702e05091c7b0139531cb5edac212a28d7bf8a63
Jan Miksovsky <jan@quickui.org>
2017-10-04 11:12:50 -0700

    Experiment with populating template into light DOM.

---
+++ src/components/TestElement2.js
@@ -0,0 +1,87 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { formatStyleProps, mergeDeep } from '../mixins/helpers.js';
+import AttributeMarshallingMixin from '../mixins/AttributeMarshallingMixin.js';
+// import ChildrenContentMixin from '../mixins/ChildrenContentMixin.js';
+import LitHtmlMixin from '../mixins/LitHtmlMixin.js';
+import ReactiveMixin from '../mixins/ReactiveMixin.js';
+
+
+const Base =
+  AttributeMarshallingMixin(
+  // ChildrenContentMixin(
+  LitHtmlMixin(
+  ReactiveMixin(
+    HTMLElement
+  )));
+
+
+/**
+ * A simple web component created with a functional reactive programming (FRP)
+ * style. In this approach, we track component state in a single `state` member,
+ * then render that state to DOM. For that task, the component uses lit-html,
+ * although other similar libraries could be used instead.
+ *
+ * The component itself is a trivial "Hello, world" element.
+ */
+export default class TestElement extends Base {
+
+  constructor() {
+    super();
+    // Sample event handler just to show we can respond to events.
+    this.addEventListener('click', event => {
+      this.togglePunctuation();
+    });
+  }
+
+  get defaultState() {
+    return {
+      punctuation: '.'
+    };
+  }
+  
+  // These are properties that will be applied to the element's host.
+  // Defining them this way allows other mixins to easily contribute style,
+  // ARIA, and other attributes.
+  hostProps() {
+    const punctuation = this.state.punctuation || '';
+    return mergeDeep(super.hostProps && super.hostProps(), {
+      style: {
+        'cursor': 'pointer',
+        'font-style': punctuation.match(/!/) ? 'italic' : null,
+        '-webkit-user-select': 'none',
+        'user-select': 'none',
+      }
+    });
+  }
+  
+  // A sample property that updates component state.
+  get punctuation() {
+    return this.state.punctuation;
+  }
+  set punctuation(punctuation) {
+    this.setState({ punctuation });
+  }
+  
+  // Define a template that will be used to populate the shadow subtree.
+  // This is fairly conventional FRP stuff: map component state (`this.state`)
+  // to DOM. Here we do that via lit-html. The `LitHtmlMixin` mixin
+  // actually does the work of rendering the template initially, and whenever
+  // the state changes.
+  get template() {
+    const hostProps = this.hostProps();
+    const rootStyle = formatStyleProps(hostProps.style);
+    const template = html`
+      Hello, ${this.state.children}${this.punctuation}
+    `;
+    return template;
+  }
+
+  togglePunctuation() {
+    this.punctuation = this.punctuation === '.' ?
+      '!' :
+      '.';
+  }
+}
+
+
+customElements.define('test-element', TestElement);
---
+++ src/mixins/LitHtmlMixin.js
@@ -0,0 +1,110 @@
+import { render } from '../../node_modules/lit-html/lit-html.js';
+import symbols from './symbols.js';
+
+
+const initializedKey = Symbol('initialized');
+const renderingKey = Symbol('renderingKey');
+
+
+/**
+ * Mixin for rendering a component's light DOM contents using lit-html.
+ */
+export default function LitHtmlMixin(Base) {
+  return class LitHtml extends Base {
+
+    appendChild(child) {
+      if (this[renderingKey]) {
+        super.appendChild(child);
+      } else {
+        console.log(`appendChild ${child}`);
+        const children = [...this.state.children, child];
+        this.setState({ children });
+      }
+    }
+
+    get innerHTML() {
+      if (this[renderingKey]) {
+        return super.innerHTML;
+      } else {
+        const strings = this.state.children.map(o =>
+          o instanceof Element ?
+            o.outerHTML :
+              o instanceof Text ?
+                o.textContent :
+                o.toString()
+        );
+        return strings.join('');
+      }
+    }
+    set innerHTML(html) {
+      if (this[renderingKey]) {
+        super.innerHTML = html;
+      } else {
+        const template = document.createElement('template');
+        template.innerHTML = html;
+        const children = [...template.content.childNodes];
+        console.log(`set innerHTML = ${children}`);
+        this.setState({ children });
+      }
+    }
+    
+    render() {
+      if (super.render) { super.render(); }
+      
+      if (!this[initializedKey]) {
+        // First call to render
+        this[initializedKey] = true;
+        const children = extractContent(this);
+        if (children) {
+          this.setState({ children });
+          return;
+        }
+      }
+      
+      // Invoke lit-html to render the shadow subtree.
+      this[renderingKey] = true;
+      render(this.template, this);
+      this[renderingKey] = false;
+      
+      // If we've created a new shadow, let the component do other
+      // initialization based on the rendered shadow tree.
+      // if (newShadow && this[symbols.shadowCreated]) {
+        //   this[symbols.shadowCreated]();
+        // }
+    }  
+  
+    get textContent() {
+      if (this[renderingKey]) {
+        return super.textContent;
+      } else {
+        const strings = this.state.children.map(o =>
+          o instanceof Node ? o.textContent : o.toString()
+        );
+        return strings.join('');
+      }
+    }
+    set textContent(textContent) {
+      if (this[renderingKey]) {
+        super.textContent = textContent;
+      } else {
+        const children = textContent.toString();
+        console.log(`set textContent = ${children}`);
+        this.setState({ children });
+      }
+    }
+
+  }
+}
+
+
+function extractContent(component) {
+  let content = null;
+  while (component.childNodes.length > 0) {
+    if (!content) {
+      content = [];
+    }
+    content.push(component.childNodes[0]);
+    component.removeChild(component.childNodes[0]);
+  }
+  return content;
+}
